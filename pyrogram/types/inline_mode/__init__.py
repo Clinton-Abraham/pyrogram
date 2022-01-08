df75257019ab5c76e38933a19238460e35ebd157
SUBIN <64341611+subinps@users.noreply.github.com>
2022-01-08 21:40:56 +0530

    .

--- pyrogram/types/inline_mode/__init__.py
+++ pyrogram/types/inline_mode/__init__.py
@@ -24,6 +24,7 @@ from .inline_query_result_article import InlineQueryResultArticle
 from .inline_query_result_audio import InlineQueryResultAudio
 from .inline_query_result_photo import InlineQueryResultPhoto
 from .inline_query_result_video import InlineQueryResultVideo
+from .inline_query_result_cached_document import InlineQueryResultCachedDocument
 
 __all__ = [
     "InlineQuery", "InlineQueryResult", "InlineQueryResultArticle", "InlineQueryResultPhoto",
